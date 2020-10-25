6cb3f5d5f09da00119c5b6ebc11bc8a3fd138f36
huweijian5 <304797017@qq.com>
2020-10-25 23:19:58 +0800

    增加imagereader

--- app/src/main/java/com/junmeng/android_java_example/mediaprojection/MediaProjectionService.java
+++ app/src/main/java/com/junmeng/android_java_example/mediaprojection/MediaProjectionService.java
@@ -5,19 +5,32 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
 import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Message;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.WindowManager;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
 import com.junmeng.android_java_example.R;
+
+import java.nio.ByteBuffer;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class MediaProjectionService extends Service {
@@ -30,9 +43,31 @@ public class MediaProjectionService extends Service {
 
     private IBinder binder;
 
+    private HandlerThread handlerThread;
+    private Handler threadHandler;
+    private Handler mainHandler;
+
+    ImageReader imageReader;
+
+    int width = 1080;
+    int height = 2172;
+
     public MediaProjectionService() {
+        initHandler();
 
     }
+    private void initHandler(){
+        handlerThread=new HandlerThread("media projection thread");
+        handlerThread.start();
+        threadHandler=new Handler(handlerThread.getLooper()){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+            }
+        };
+        mainHandler=new Handler();
+    }
+
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -48,12 +83,76 @@ public class MediaProjectionService extends Service {
         mResultData = intent.getParcelableExtra("data");
         mpm = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
         MediaProjection mediaProjection = mpm.getMediaProjection(mResultCode, mResultData);
+        WindowManager mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        mWindowManager.getDefaultDisplay().getMetrics(outMetrics);
+        width = outMetrics.widthPixels;
+        height = outMetrics.heightPixels;
+
+        //目前测试只能输出PixelFormat.RGBA_8888
+        imageReader =  ImageReader.newInstance(width,height, PixelFormat.RGBA_8888,2);
+        imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+                Log.i(TAG, "onImageAvailable: reader==null? "+(reader==null));
+                //获取最新的一帧的Image
+                Image image = reader.acquireLatestImage();
+                if(image==null){
+
+                    return;
+                }
+//因为是ImageFormat.JPEG格式，所以 image.getPlanes()返回的数组只有一个，也就是第0个。
+                ByteBuffer byteBuffer = image.getPlanes()[0].getBuffer();
+                byte[] bytes = new byte[byteBuffer.remaining()];
+                byteBuffer.get(bytes);
+                Log.i(TAG, "onImageAvailable: len="+bytes.length);
+
+//                Image.Plane[] planes = image.getPlanes();
+//                ByteBuffer buffer = planes[0].getBuffer();
+//                int pixelStride = planes[0].getPixelStride();
+//                int rowStride = planes[0].getRowStride();
+//                int rowPadding = rowStride - pixelStride * width;
+//                Bitmap bitmap = Bitmap.createBitmap(width + rowPadding / pixelStride,
+//                        height, Bitmap.Config.ARGB_8888);
+//                bitmap.copyPixelsFromBuffer(buffer);
+//                String fileName = null;
+//                try {
+//                    Date currentDate = new Date();
+//                    SimpleDateFormat date = new SimpleDateFormat("yyyyMMddhhmmss");
+//                    File dir = getExternalFilesDir(null);
+//                    fileName = dir.getAbsolutePath() + "/" + date.format(currentDate) + ".png";
+//                    FileOutputStream fos = new FileOutputStream(fileName);
+//                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+//                    fos.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+                image.close();
+
+            }
+        },threadHandler);
+        mediaProjection.createVirtualDisplay("media projection", imageReader.getWidth(),imageReader.getHeight() , outMetrics.densityDpi,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR, imageReader.getSurface(), new VirtualDisplay.Callback() {
+                    @Override
+                    public void onPaused() {
+                        Log.i(TAG, "onPaused: ");
+                    }
+
+                    @Override
+                    public void onResumed() {
+                        Log.i(TAG, "onResumed: ");
+                    }
+
+                    @Override
+                    public void onStopped() {
+                        Log.i(TAG, "onStopped: ");
+                    }
+                }, null);
 
 
         return super.onStartCommand(intent, flags, startId);
 
     }
-
 
 
     private void createNotificationChannel() {
@@ -88,8 +187,8 @@ public class MediaProjectionService extends Service {
             builder.setContentIntent(pendingIntent);
             Notification notification = builder.build();
             startForeground(100000, notification);
-            Log.d(TAG,"Start foreground service");
-        }else{
+            Log.d(TAG, "Start foreground service");
+        } else {
             // 在API16之后，可以使用build()来进行Notification的构建 Notification
             Notification notification = new Notification.Builder(this.getApplicationContext())
                     .setContentText("这是一个前台服务")
