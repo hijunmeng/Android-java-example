cae7f458b1ad8a9b8d0a79f90524b113d84832e8
huweijian5 <304797017@qq.com>
2020-12-21 23:53:42 +0800

    增加自定义对话框

---
+++ ui/src/main/java/com/example/ui/dialog/CommonDialogFragment.java
@@ -0,0 +1,526 @@
+package com.example.ui.dialog;
+
+import android.content.DialogInterface;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.DrawableRes;
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.ui.R;
+
+/**
+ * 通用对话框（必含内容和一个按钮）
+ * 支持自定义布局，保持id一致即可
+ * 支持三种类型按钮
+ * 支持标题
+ * 支持关闭按钮
+ */
+public class CommonDialogFragment extends DialogFragment implements View.OnClickListener, DialogInterface {
+    private static final String TAG = "CommonDialogFragment";
+    private Builder mBuilder;
+
+    //contentView和positiveView不能为空
+    @Nullable
+    private TextView titleView;
+    private TextView contentView;
+    @Nullable
+    private TextView negativeView;
+    @Nullable
+    private TextView neutralView;
+    private TextView positiveView;
+    @Nullable
+    private ImageView closeView;
+    @Nullable
+    private View splite1View;
+    @Nullable
+    private View splite2View;
+
+    public CommonDialogFragment(@NonNull Builder builder) {
+        mBuilder = builder;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(mBuilder.layoutResId, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        titleView = view.findViewById(R.id.title);
+        closeView = view.findViewById(R.id.close);
+        contentView = view.findViewById(R.id.content);
+        negativeView = view.findViewById(R.id.negative);
+        positiveView = view.findViewById(R.id.positive);
+        neutralView = view.findViewById(R.id.neutral);
+        splite1View = view.findViewById(R.id.splite1);
+        splite2View = view.findViewById(R.id.splite2);
+
+        handleCloseButton();
+
+        handleTitleView();
+
+        handleContentView();
+
+        handlePositiveButton();
+        handleNegativeButton();
+        handleNeutralButton();
+
+        handleSpliteView();
+    }
+
+    private void handleContentView() {
+        if (mBuilder.isContentCenter) {
+            contentView.setGravity(Gravity.CENTER);
+        }
+        if (mBuilder.contentSizeSP != -1) {
+            contentView.setTextSize(TypedValue.COMPLEX_UNIT_SP, mBuilder.contentSizeSP);
+        }
+        if (mBuilder.contentColor != -1) {
+            contentView.setTextColor(mBuilder.contentColor);
+        }
+        contentView.setText(mBuilder.content);
+    }
+
+    private void handleCloseButton() {
+        if (closeView == null) {
+            return;
+        }
+        if (mBuilder.isShowCloseButton) {
+            closeView.setVisibility(View.VISIBLE);
+            closeView.setOnClickListener(this);
+            if (mBuilder.closeIcon != -1) {
+                closeView.setImageResource(mBuilder.closeIcon);
+            }
+        } else {
+            closeView.setVisibility(View.GONE);
+        }
+
+    }
+
+    private void handleSpliteView() {
+        int flag = (mBuilder.isShowPositive ? 1 : 0)
+                + (mBuilder.isShowNegative ? 1 : 0)
+                + (mBuilder.isShowNeutral ? 1 : 0);
+        if (flag == 3) {
+            if (splite1View != null) {
+                splite1View.setVisibility(View.VISIBLE);
+            }
+            if (splite2View != null) {
+                splite2View.setVisibility(View.VISIBLE);
+            }
+        } else if (flag == 2) {
+            if (splite1View != null) {
+                splite1View.setVisibility(View.VISIBLE);
+            }
+            if (splite2View != null) {
+                splite2View.setVisibility(View.GONE);
+            }
+
+        } else {
+            if (splite1View != null) {
+                splite1View.setVisibility(View.GONE);
+            }
+            if (splite2View != null) {
+                splite2View.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    private void handleNeutralButton() {
+        if (neutralView == null) {
+            return;
+        }
+
+        if (mBuilder.isShowNeutral) {
+            neutralView.setOnClickListener(this);
+            neutralView.setVisibility(View.VISIBLE);
+            if (mBuilder.positiveButtonText != null) {
+                neutralView.setText(mBuilder.neutralButtonText);
+            }
+            if (mBuilder.positiveButtonColor != -1) {
+                neutralView.setTextColor(mBuilder.neutralButtonColor);
+            }
+            if (mBuilder.buttonSizeSP != -1) {
+                neutralView.setTextSize(TypedValue.COMPLEX_UNIT_SP, mBuilder.buttonSizeSP);
+            }
+
+        } else {
+            neutralView.setVisibility(View.GONE);
+        }
+    }
+
+    private void handleNegativeButton() {
+        if (negativeView == null) {
+            return;
+        }
+        if (mBuilder.isShowNegative) {
+            negativeView.setOnClickListener(this);
+            negativeView.setVisibility(View.VISIBLE);
+            if (mBuilder.positiveButtonText != null) {
+                negativeView.setText(mBuilder.negativeButtonText);
+            }
+            if (mBuilder.positiveButtonColor != -1) {
+                negativeView.setTextColor(mBuilder.negativeButtonColor);
+            }
+            if (mBuilder.buttonSizeSP != -1) {
+                negativeView.setTextSize(TypedValue.COMPLEX_UNIT_SP, mBuilder.buttonSizeSP);
+            }
+
+        } else {
+            negativeView.setVisibility(View.GONE);
+        }
+    }
+
+    private void handlePositiveButton() {
+        if (mBuilder.isShowPositive) {
+            positiveView.setOnClickListener(this);
+            positiveView.setVisibility(View.VISIBLE);
+            if (mBuilder.positiveButtonText != null) {
+                positiveView.setText(mBuilder.positiveButtonText);
+            }
+            if (mBuilder.positiveButtonColor != -1) {
+                positiveView.setTextColor(mBuilder.positiveButtonColor);
+            }
+            if (mBuilder.buttonSizeSP != -1) {
+                positiveView.setTextSize(TypedValue.COMPLEX_UNIT_SP, mBuilder.buttonSizeSP);
+            }
+
+        } else {
+            positiveView.setVisibility(View.GONE);
+        }
+    }
+
+    private void handleTitleView() {
+        if (titleView == null) {
+            return;
+        }
+        if (mBuilder.isShowTitle) {
+            titleView.setVisibility(View.VISIBLE);
+            if (mBuilder.titleSizeSP != -1) {
+                titleView.setTextSize(TypedValue.COMPLEX_UNIT_SP, mBuilder.titleSizeSP);
+            }
+            if (mBuilder.titleBold) {
+                titleView.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+            }
+            if (mBuilder.titleColor != -1) {
+                titleView.setTextColor(mBuilder.titleColor);
+            }
+            titleView.setText(mBuilder.title);
+        } else {
+            titleView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mBuilder.isAutoDismiss) {
+            dismiss();
+        }
+        int id = v.getId();
+        if (id == R.id.close) {
+            if (isVisible()) {
+                dismiss();
+            }
+        } else if (id == R.id.negative) {
+            if (mBuilder.negativeClickListener != null) {
+                mBuilder.negativeClickListener.onClick(this, DialogInterface.BUTTON_NEGATIVE);
+            }
+        } else if (id == R.id.positive) {
+            if (mBuilder.positiveClickListener != null) {
+                mBuilder.positiveClickListener.onClick(this, DialogInterface.BUTTON_POSITIVE);
+            }
+        } else if (id == R.id.neutral) {
+            if (mBuilder.neutralClickListener != null) {
+                mBuilder.neutralClickListener.onClick(this, DialogInterface.BUTTON_NEUTRAL);
+            }
+        }
+    }
+
+    @Override
+    public void cancel() {
+        dismiss();
+    }
+
+    public static class Builder implements IDialog {
+        private String title = null;
+        private int titleColor = -1;
+        private int titleSizeSP = -1;
+        private boolean titleBold = false;
+        private boolean isShowTitle = false;
+
+        private boolean isContentCenter = false;
+        private String content = null;
+        private int contentColor = -1;
+        private int contentSizeSP = -1;
+
+
+        private int buttonSizeSP = -1;//-1表示默认
+        private boolean isAutoDismiss = true;
+        private String negativeButtonText = null;
+        private int negativeButtonColor = -1;
+        private DialogInterface.OnClickListener negativeClickListener;
+        private boolean isShowNegative;
+
+        private String neutralButtonText = null;
+        private int neutralButtonColor = -1;
+        private DialogInterface.OnClickListener neutralClickListener;
+        private boolean isShowNeutral;
+
+        private String positiveButtonText = null;
+        private int positiveButtonColor = -1;
+        private DialogInterface.OnClickListener positiveClickListener;
+        private boolean isShowPositive = true;
+
+        private boolean isCancelable = true;
+
+
+        private int layoutResId = R.layout.junmeng_dialog_simple;
+        private boolean isShowCloseButton = false;
+        private int closeIcon = -1;
+
+
+        @Override
+        public Builder setTitle(String title) {
+            isShowTitle = true;
+            this.title = title;
+            return this;
+        }
+
+        @Override
+        public Builder setTitleColor(@ColorInt int titleColor) {
+            this.titleColor = titleColor;
+            return this;
+        }
+
+        @Override
+        public Builder setTitleSize(int titleSizeSP) {
+            this.titleSizeSP = titleSizeSP;
+            return this;
+        }
+
+        @Override
+        public Builder setTitleBold(boolean titleBold) {
+            this.titleBold = titleBold;
+            return this;
+        }
+
+        @Override
+        public Builder isContentCenter(boolean isContentCenter) {
+            this.isContentCenter = isContentCenter;
+            return this;
+        }
+
+        @Override
+        public Builder setContent(String content) {
+            this.content = content;
+            return this;
+        }
+
+        @Override
+        public Builder setContentColor(@ColorInt int contentColor) {
+            this.contentColor = contentColor;
+            return this;
+        }
+
+        @Override
+        public Builder setContentSize(int contentSizeSP) {
+            this.contentSizeSP = contentSizeSP;
+            return this;
+        }
+
+        @Override
+        public Builder setButtonSize(int buttonSizeSP) {
+            this.buttonSizeSP = buttonSizeSP;
+            return this;
+        }
+
+        @Override
+        public Builder setPositiveButton(String text, @ColorInt int color, @Nullable DialogInterface.OnClickListener onClickListener) {
+            isShowPositive = true;
+            this.positiveClickListener = onClickListener;
+            this.positiveButtonColor = color;
+            this.positiveButtonText = text;
+            return this;
+        }
+
+        @Override
+        public Builder setNeutralButton(String text, @ColorInt int color, @Nullable DialogInterface.OnClickListener onClickListener) {
+            isShowNeutral = true;
+            this.neutralClickListener = onClickListener;
+            this.neutralButtonColor = color;
+            this.neutralButtonText = text;
+            return this;
+        }
+
+        @Override
+        public Builder setNegativeButton(String text, @ColorInt int color, @Nullable DialogInterface.OnClickListener onClickListener) {
+            isShowNegative = true;
+            this.negativeClickListener = onClickListener;
+            this.negativeButtonColor = color;
+            this.negativeButtonText = text;
+            return this;
+        }
+
+        @Override
+        public Builder setCloseIcon(@DrawableRes int iconResId) {
+            this.closeIcon = iconResId;
+            return this;
+        }
+
+        @Override
+        public Builder isShowCloseButton(boolean isShowCloseButton) {
+            this.isShowCloseButton = isShowCloseButton;
+            return this;
+        }
+
+        @Override
+        public Builder isAutoDismiss(boolean isAutoDismiss) {
+            this.isAutoDismiss = isAutoDismiss;
+            return this;
+        }
+
+        @Override
+        public Builder setCancelable(boolean cancelable) {
+            this.isCancelable = cancelable;
+            return this;
+        }
+
+        @Override
+        public Builder setLayout(int layoutResId) {
+            this.layoutResId = layoutResId;
+            return this;
+        }
+
+        public CommonDialogFragment create() {
+            if (TextUtils.isEmpty(content)) {
+                throw new RuntimeException("必须调用setContent");
+            }
+            CommonDialogFragment dialog = new CommonDialogFragment(this);
+            dialog.setCancelable(isCancelable);
+            return dialog;
+        }
+
+    }
+
+    public interface IDialog {
+        /**
+         * 标题文本
+         *
+         * @return
+         */
+        Builder setTitle(@NonNull String title);
+
+        /**
+         * 标题颜色
+         *
+         * @param titleColor
+         * @return
+         */
+        Builder setTitleColor(@ColorInt int titleColor);
+
+        /**
+         * 标题大小
+         *
+         * @param titleSizeSP
+         * @return
+         */
+        Builder setTitleSize(int titleSizeSP);
+
+        /**
+         * 标题是否加粗
+         *
+         * @param titleBold
+         * @return
+         */
+        Builder setTitleBold(boolean titleBold);
+
+        /**
+         * 内容是否居中，默认left
+         *
+         * @param isContentCenter
+         * @return
+         */
+        Builder isContentCenter(boolean isContentCenter);
+
+        Builder setContent(@NonNull String content);
+
+        Builder setContentColor(@ColorInt int contentColor);
+
+        Builder setContentSize(int contentSizeSP);
+
+        /**
+         * 设置按钮文字大小
+         *
+         * @param buttonSizeSP
+         * @return
+         */
+        Builder setButtonSize(int buttonSizeSP);
+
+        /**
+         * @param text            为null表示使用默认
+         * @param color           -1表示使用默认
+         * @param onClickListener
+         * @return
+         */
+        Builder setPositiveButton(@Nullable String text, @ColorInt int color, @Nullable DialogInterface.OnClickListener onClickListener);
+
+        Builder setNeutralButton(@Nullable String text, @ColorInt int color, @Nullable DialogInterface.OnClickListener onClickListener);
+
+        Builder setNegativeButton(@Nullable String text, @ColorInt int color, @Nullable DialogInterface.OnClickListener onClickListener);
+
+        /**
+         * 设置关闭图标
+         *
+         * @param iconResId
+         * @return
+         */
+        Builder setCloseIcon(@DrawableRes int iconResId);
+
+        /**
+         * 是否展示关闭按钮
+         *
+         * @param isShowCloseButton
+         * @return
+         */
+        Builder isShowCloseButton(boolean isShowCloseButton);
+
+        /**
+         * 在点击时对话框是否自动消失，默认true
+         *
+         * @param isAutoDismiss
+         * @return
+         */
+        Builder isAutoDismiss(boolean isAutoDismiss);
+
+        /**
+         * 点击外部是否会关闭对话框
+         *
+         * @param cancelable
+         * @return
+         */
+        Builder setCancelable(boolean cancelable);
+
+        /**
+         * 自定义布局
+         *
+         * @param layoutResId
+         * @return
+         */
+        Builder setLayout(@LayoutRes int layoutResId);
+    }
+}
--- ui/src/main/java/com/example/ui/widget/LineWaveVoiceView.java
+++ ui/src/main/java/com/example/ui/widget/LineWaveVoiceView.java
@@ -6,7 +6,6 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.IntRange;
@@ -20,7 +19,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * 语音录制音量分贝的动画效果
+ * 语音录制时音量的跳动效果
+ * 支持两种动画模式
+ * 支持自定义线条颜色间距等
  */
 public class LineWaveVoiceView extends View {
     private static final String TAG = "LineWaveVoiceView";
@@ -28,26 +29,31 @@ public class LineWaveVoiceView extends View {
     private Paint paint = new Paint();
     private Runnable task;
     private ExecutorService executorService = Executors.newCachedThreadPool();
-    private RectF rectRight = new RectF();//右边波纹矩形的数据
+    private RectF rectRight = new RectF();//右边波纹矩形的数据，10个矩形复用一个rectF
     private RectF rectLeft = new RectF();//左边波纹矩形的数据
     private int updateSpeed;
 
-    private static final int DEFAULT_LINE_WIDTH = 9;//默认矩形线的宽度
-    private static final int DEFAULT_LINE_SPACE = 9;//默认矩形线之间的间隔
-    private static final int DEFAULT_MIN_WAVE_HEIGHT = 2;//默认矩形线最小高
-    private static final int DEFAULT_MAX_WAVE_HEIGHT = 12;//默认矩形线最大高
+    private static final int DEFAULT_LINE_ROUND = 6; //矩形圆角
+    private static final int DEFAULT_LINE_WIDTH = 9;//默认矩形波纹的宽度，9像素, 原则上从layout的attr获得
+    private static final int DEFAULT_LINE_SPACE = 9;//默认矩形波纹的宽度，9像素, 原则上从layout的attr获得
+    private static final int DEFAULT_MIN_WAVE_HEIGHT = 2;//矩形线最小高
+    private static final int DEFAULT_MAX_WAVE_HEIGHT = 12;//矩形线最大高
     private static final int UPDATE_INTERVAL_TIME = 100;//100ms更新一次
-    private static final int DEFAULT_LINE_HLAF_COUNT = 7;//默认左右两边的矩形线个数（总数为DEFAULT_LINE_HLAF_COUNT*2）
-    private LinkedList<Integer> mWaveList = new LinkedList<>();//存放分贝级别，最新的分贝级别放在0位置
-    private float mRandomDb;//随机产生的分贝
+    private static final int DEFAULT_LINE_HLAF_COUNT = 7;//矩形条数的一半
+    private LinkedList<Integer> mWaveList = new LinkedList<>();
+    private float maxDb;
 
     private int lineHalfCount = DEFAULT_LINE_HLAF_COUNT; //左右两边的线条数
     private float minLineHeight = DEFAULT_MIN_WAVE_HEIGHT;//矩形线最小高
     private float maxLineHeight = DEFAULT_MAX_WAVE_HEIGHT;//矩形线最大高
-    private float lineSpace;//线与线之间的间隔
+    private float lineSpace = DEFAULT_LINE_SPACE;//线与线之间的间隔
     private float lineWidth = DEFAULT_LINE_WIDTH;//矩形线的宽度
     private int lineColor = DEFAULT_LINE_SPACE;//矩形线的颜色
+    private int lineRound = DEFAULT_LINE_ROUND;//矩形的圆角
 
+    private static final int MODE_FROM_CENTER_TO_EDGE = 1;// 从中间向两边扩散
+    private static final int MODE_FROM_LEFT_TO_RIGHT = 2;// 从左到右扩散
+    private int mode = MODE_FROM_CENTER_TO_EDGE;
 
     public LineWaveVoiceView(Context context) {
         super(context);
@@ -60,10 +66,21 @@ public class LineWaveVoiceView extends View {
     public LineWaveVoiceView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initView(attrs, context);
-        int[] tmp = new int[lineHalfCount];
-        for (int i = 0; i < lineHalfCount; i++) {
-            tmp[i] = (int) minLineHeight;
+        int[] tmp;
+        if (mode == MODE_FROM_LEFT_TO_RIGHT) {
+            tmp = new int[lineHalfCount * 2];
+            for (int i = 0; i < lineHalfCount * 2; i++) {
+                tmp[i] = (int) minLineHeight;
+            }
+            resetView(mWaveList, tmp);
+        } else {
+            tmp = new int[lineHalfCount];
+            for (int i = 0; i < lineHalfCount; i++) {
+                tmp[i] = (int) minLineHeight;
+            }
+
         }
+
         resetView(mWaveList, tmp);
         task = new LineJitterTask();
     }
@@ -71,13 +88,15 @@ public class LineWaveVoiceView extends View {
     private void initView(AttributeSet attrs, Context context) {
         //获取布局属性里的值
         TypedArray mTypedArray = context.obtainStyledAttributes(attrs, R.styleable.LineWaveVoiceView);
-        lineColor = mTypedArray.getColor(R.styleable.LineWaveVoiceView_voiceLineColor, context.getResources().getColor(android.R.color.white));
-        lineWidth = mTypedArray.getDimension(R.styleable.LineWaveVoiceView_voiceLineWidth, DEFAULT_LINE_WIDTH);
-        lineSpace = mTypedArray.getDimension(R.styleable.LineWaveVoiceView_lineSpaceWidth, DEFAULT_LINE_SPACE);
-        minLineHeight = mTypedArray.getDimension(R.styleable.LineWaveVoiceView_minLineHeight, DEFAULT_MIN_WAVE_HEIGHT);
-        maxLineHeight = mTypedArray.getDimension(R.styleable.LineWaveVoiceView_maxLineHeight, DEFAULT_MAX_WAVE_HEIGHT);
-        lineHalfCount = mTypedArray.getInt(R.styleable.LineWaveVoiceView_lineHalfCount, DEFAULT_LINE_HLAF_COUNT);
-        updateSpeed = mTypedArray.getColor(R.styleable.LineWaveVoiceView_updateSpeed, UPDATE_INTERVAL_TIME);
+        lineColor = mTypedArray.getColor(R.styleable.LineWaveVoiceView_junmeng_voiceLineColor, context.getResources().getColor(android.R.color.white));
+        lineWidth = mTypedArray.getDimension(R.styleable.LineWaveVoiceView_junmeng_voiceLineWidth, DEFAULT_LINE_WIDTH);
+        lineSpace = mTypedArray.getDimension(R.styleable.LineWaveVoiceView_junmeng_lineSpaceWidth, DEFAULT_LINE_SPACE);
+        minLineHeight = mTypedArray.getDimension(R.styleable.LineWaveVoiceView_junmeng_minLineHeight, DEFAULT_MIN_WAVE_HEIGHT);
+        maxLineHeight = mTypedArray.getDimension(R.styleable.LineWaveVoiceView_junmeng_maxLineHeight, DEFAULT_MAX_WAVE_HEIGHT);
+        lineHalfCount = mTypedArray.getInt(R.styleable.LineWaveVoiceView_junmeng_lineHalfCount, DEFAULT_LINE_HLAF_COUNT);
+        updateSpeed = mTypedArray.getColor(R.styleable.LineWaveVoiceView_junmeng_updateSpeed, UPDATE_INTERVAL_TIME);
+        mode = mTypedArray.getInt(R.styleable.LineWaveVoiceView_junmeng_mode, MODE_FROM_CENTER_TO_EDGE);
+        lineRound = mTypedArray.getInt(R.styleable.LineWaveVoiceView_junmeng_lineRound, DEFAULT_LINE_ROUND);
         mTypedArray.recycle();
     }
 
@@ -93,6 +112,22 @@ public class LineWaveVoiceView extends View {
         paint.setStyle(Paint.Style.FILL);
         //设置抗锯齿
         paint.setAntiAlias(true);
+
+        if (mode == MODE_FROM_LEFT_TO_RIGHT) {
+            fromLeftToRight(canvas, widthCentre, heightCentre);
+        } else {
+            fromCenterToEdge(canvas, widthCentre, heightCentre);
+        }
+    }
+
+    /**
+     * 从中间两边扩散
+     *
+     * @param canvas
+     * @param widthCentre
+     * @param heightCentre
+     */
+    private void fromCenterToEdge(Canvas canvas, int widthCentre, int heightCentre) {
         for (int i = 0; i < lineHalfCount; i++) {
             rectRight.left = widthCentre + (0.5f + i) * lineSpace + (i) * lineWidth;
             rectRight.top = heightCentre - mWaveList.get(i) / 2;
@@ -105,11 +140,40 @@ public class LineWaveVoiceView extends View {
             rectLeft.right = rectLeft.left + lineWidth;
             rectLeft.bottom = heightCentre + mWaveList.get(i) / 2;
 
-            canvas.drawRoundRect(rectRight, 6, 6, paint);
-            canvas.drawRoundRect(rectLeft, 6, 6, paint);
+            canvas.drawRoundRect(rectRight, lineRound, lineRound, paint);
+            canvas.drawRoundRect(rectLeft, lineRound, lineRound, paint);
         }
     }
 
+    /**
+     * 从左边到右边
+     *
+     * @param canvas
+     * @param widthCentre
+     * @param heightCentre
+     */
+    private void fromLeftToRight(Canvas canvas, int widthCentre, int heightCentre) {
+        int index = 0;
+        for (int i = lineHalfCount; i > 0; i--) {
+            //左边矩形
+            rectLeft.left = widthCentre - (i * lineWidth + (i - 0.5f) * lineSpace);
+            rectLeft.top = heightCentre - mWaveList.get(index) / 2;
+            rectLeft.right = rectLeft.left + lineWidth;
+            rectLeft.bottom = heightCentre + mWaveList.get(index) / 2;
+
+            canvas.drawRoundRect(rectLeft, lineRound, lineRound, paint);
+            index++;
+        }
+        for (int i = 1; i <= lineHalfCount; i++) {
+            //右边矩形
+            rectRight.left = widthCentre + ((i - 1) * lineWidth + (i - 0.5f) * lineSpace);
+            rectRight.top = heightCentre - mWaveList.get(index) / 2;
+            rectRight.right = rectRight.left + lineWidth;
+            rectRight.bottom = heightCentre + mWaveList.get(index) / 2;
+            canvas.drawRoundRect(rectRight, lineRound, lineRound, paint);
+            index++;
+        }
+    }
 
     private void resetView(List<Integer> list, int[] array) {
         list.clear();
@@ -120,8 +184,8 @@ public class LineWaveVoiceView extends View {
 
     private synchronized void refreshElement() {
         Random random = new Random();
-        mRandomDb = random.nextInt(5) + 2;
-        int waveH = (int) (minLineHeight + Math.round(mRandomDb / 7.0f * (maxLineHeight - minLineHeight)));
+        maxDb = random.nextInt(5) + 2;
+        int waveH = (int) (minLineHeight + Math.round(maxDb / 7.0f * (maxLineHeight - minLineHeight)));
         mWaveList.add(0, waveH);
         mWaveList.removeLast();
     }
@@ -143,15 +207,13 @@ public class LineWaveVoiceView extends View {
         }
     }
 
-
     public synchronized void refreshElement(@IntRange(from = 0, to = 6) int dbLevel) {
         int waveH = (int) (minLineHeight + Math.round(dbLevel / 6.0f * (maxLineHeight - minLineHeight)));
-        Log.i(TAG, "db=" + dbLevel + ",waveH=" + waveH);
+//        Log.i(TAG, "db=" + dbLevel + ",waveH=" + waveH);
         mWaveList.add(0, waveH);
         mWaveList.removeLast();
         postInvalidate();
     }
-
 
     public void setUpdateSpeed(int updateSpeed) {
         this.updateSpeed = updateSpeed;
